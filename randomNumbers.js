29053b43e5b0593adec0503777aad964d54f626d
Veljko-pop_os <veljkoloncarevicharry@gmail.com>
2021-10-27 00:26:56 +0200

    Added some comments and a readme.md file

--- randomNumbers.js
+++ randomNumbers.js
@@ -3,7 +3,8 @@ let x1 = 0;
 function randomNumber() {
     do {
         x = Math.floor(Math.random() * 10 + 1);
-    }
+    } // Make sure we get either a lower or a higher number,
+      // generate a random number again if it's equal to the previous one
     while (x === x1);
     document.getElementById("numberDisplay").innerHTML = x;
     x1 = x;
@@ -14,7 +15,9 @@ function addOrRemoveMoney(operation) {
     let money = Number(document.getElementById("money").textContent);
     // If the condition is true, add more money to the total,
     // otherwise subtract.
-    // This function is used in hiloCheck()
+    // This function is used in hiloCheck(),
+    // Where we pass "true" as the argument
+    // if the guess is correct.
     if (operation) {
         document.getElementById("tekst").innerHTML = "Correct!";
         money += 50;
@@ -30,18 +33,24 @@ function hiloCheck(hilo) {
     randomNumber();
     let money = Number(document.getElementById("money").textContent);
     switch (hilo) {
+        // case 1 is for when the player chooses Higher
         case 1:
             if (x > x2) {
+                // The guess is correct
                 addOrRemoveMoney(true);
             }
             else {
+                // The guess is incorrect
                 addOrRemoveMoney(false)
             }
             break;
+        // case 2 is for when the player chooses Lower
         case 2:
             if (x < x2) {
+                // The guess is correct
                 addOrRemoveMoney(true);
             } else {
+                // The guess is incorrect
                 addOrRemoveMoney(false);
             }
             break;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# A very simple Hi-Lo game
+Written in HTML, CSS and Javascript. 
+
+**How to play**
+You'll see a randomly generated number from 1 to 10 in the white field on the screen. Your goal is to (correctly) guess whether the next randomly generated number will be higher or lower than the currently displayed one. You'll start out with $500 in the bank. Try to make as much money as you can!
